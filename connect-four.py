f9c645f241475896aa84e7c039f3914fcabd59f9
Mitchell <mitchellmorley@gmail.com>
2023-02-07 22:14:29 -0800

    refactor grid and print grid, add function to place token

--- connect-four.py
+++ connect-four.py
@@ -3,13 +3,13 @@
 # Initializing The Grid. A 7 x 6 grid where tokens can go
 
 grid = [
-    ['[ ]','[ ]','[ ]','[ ]','[ ]','[ ]'],
-    ['[ ]','[ ]','[ ]','[ ]','[ ]','[ ]'],
-    ['[ ]','[ ]','[ ]','[ ]','[ ]','[ ]'],
-    ['[ ]','[ ]','[ ]','[ ]','[ ]','[ ]'],
-    ['[ ]','[ ]','[ ]','[ ]','[ ]','[ ]'],
-    ['[ ]','[ ]','[ ]','[ ]','[ ]','[ ]'],
-    ['[ ]','[ ]','[ ]','[ ]','[ ]','[ ]'],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
 ]
 
 ## System 1.5: Printing The Grid
@@ -17,29 +17,34 @@ grid = [
 def print_grid():
     # Initialize Print Grid
     print_grid = [
-        ['','','','','','',''],
-        ['','','','','','',''],
-        ['','','','','','',''],
-        ['','','','','','',''],
-        ['','','','','','',''],
-        ['','','','','','',''],
+        ['[ ]','[ ]','[ ]','[ ]','[ ]','[ ]', '[ ]'],
+        ['[ ]','[ ]','[ ]','[ ]','[ ]','[ ]', '[ ]'],
+        ['[ ]','[ ]','[ ]','[ ]','[ ]','[ ]', '[ ]'],
+        ['[ ]','[ ]','[ ]','[ ]','[ ]','[ ]', '[ ]'],
+        ['[ ]','[ ]','[ ]','[ ]','[ ]','[ ]', '[ ]'],
+        ['[ ]','[ ]','[ ]','[ ]','[ ]','[ ]', '[ ]'],
     ]
     for i, column in enumerate(grid):
         for j, cell in enumerate(column):
             print_grid[j][i] = cell
     
-    for row in print_grid:
+    for row in reversed(print_grid):
         for cell in row:
             print(cell, end=' ')
         print()
 
 
+# let's assume the input is valid, for now
 
-## Second System: Placing a token. Will only be called with 'black' or 'red' as the color param
-
-def place_token(color, x, y):
-    pass
+def place_token(color, col):
+    grid[col].append('[' + color + ']')
 
 
 # Testing print_grid function
+
+place_token('X', 0)
+place_token('X', 0)
+place_token('X', 0)
+place_token('O', 2)
+
 print_grid()
